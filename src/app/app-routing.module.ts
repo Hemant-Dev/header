77739f41d22d8815de0c64f54b669b9c082e30f9
Hemant-Dev <phemant092@gmail.com>
2024-05-07 18:04:39 +0530

    header-mfe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TitleComponent } from './title/title.component';
+import { APP_BASE_HREF } from '@angular/common';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'menu',
+    component: TitleComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/header' }],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
